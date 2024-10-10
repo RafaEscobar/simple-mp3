3d60f5ae3f4eb05937b00d4f2d3fc100ac82f375
Rafa Escobar <rafael.eg.code@gmail.com>
2024-10-10 15:08:46 -0600

    Agrega widget de emptyState y separa logica de la UI en player screen

--- lib/screens/player_screen.dart
+++ lib/screens/player_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:simple_mp3/services/preferences.dart';
 import 'package:simple_mp3/utils/alert.dart';
 import 'package:simple_mp3/utils/local_permission.dart';
+import 'package:simple_mp3/widgets/empty_state.dart';
 
 class PlayerScreen extends StatefulWidget {
-  const PlayerScreen({super.key});
   static const String routeName = 'playerSPlayerScreen-screen';
+  const PlayerScreen({super.key});
 
   @override
   State<PlayerScreen> createState() => _PlayerScreenState();
@@ -15,6 +15,17 @@ class PlayerScreen extends StatefulWidget {
 
 class _PlayerScreenState extends State<PlayerScreen> {
   late PermissionStatus showNoPermission;
+
+  Future<void> requestPermission() async {
+    if (Preferences.storagePermissionResponse.isPermanentlyDenied) {
+      Alert.showBasicAlert('A continuación seras redirigido a la configuración de la aplicación, ahi podras conceder el permiso para acceder al almacenamiento interno');
+      await Future.delayed(const Duration(seconds: 5));
+      openAppSettings();
+    } else {
+      PermissionStatus statusRequest = await LocalPermission.requestPermission();
+      Preferences.storagePermissionResponse = statusRequest;
+    }
+  }
 
   @override
   void initState() {
@@ -36,24 +47,12 @@ class _PlayerScreenState extends State<PlayerScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text(
-                  'Necesitamos permiso para buscar tu música',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                const EmptyState(
+                  lottiePath: 'assets/animations/empty_state.json',
+                  title: 'Necesitamos permiso para buscar tu música'
                 ),
-                const SizedBox(height: 10,),
-                Lottie.asset('assets/animations/empty_state.json'),
-                const SizedBox(height: 10,),
                 ElevatedButton(
-                  onPressed: () async {
-                    if (Preferences.storagePermissionResponse.isPermanentlyDenied) {
-                      Alert.showBasicAlert('A continuación seras redirigido a la configuración de la aplicación, ahi podras conceder el permiso para acceder al almacenamiento interno');
-                      await Future.delayed(const Duration(seconds: 5));
-                      openAppSettings();
-                    } else {
-                      PermissionStatus statusRequest = await LocalPermission.requestPermission();
-                      Preferences.storagePermissionResponse = statusRequest;
-                    }
-                  },
+                  onPressed: requestPermission,
                   child: const Text('Conceder permiso')
                 )
               ],
---
+++ lib/widgets/empty_state.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class EmptyState extends StatelessWidget{
+  final String lottiePath;
+  final String title;
+  final String subtitle;
+  const EmptyState({
+    super.key,
+    required this.lottiePath,
+    required this.title,
+    this.subtitle = ''
+  });
+
+
+  @override
+  Widget build(BuildContext context){
+    return SizedBox(
+      child: Column(
+        children: [
+          Lottie.asset(lottiePath),
+          const SizedBox(height: 10,),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          const SizedBox(height: 6,),
+          Text(
+            subtitle,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w200
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
